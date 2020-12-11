8f4ff5a481184145f3f760b9bb07bf92877340f7
unya33ki <iimuzoku2@yahoo.co.jp>
2020-12-11 12:03:35 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index' 
-    # get "search", to: "posts#search"
-  resources :posts, only: :index do
-    collection do
-      get "search"
-    end
-  
-  get "posts/new", to: "posts#new"
+  root to: 'posts#index' 
   post "posts", to: "posts#create"
-  # member do get "search" end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
